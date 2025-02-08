cdb8e1cb9775176f288140db237656fb481f0b9a
rbabi207 <rozs.barbara@students.jedlik.eu>
2025-02-08 16:27:11 +0100

    st commit

---
+++ Gordeszka_sim/Gordeszka_sim/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gordeszka_sim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
